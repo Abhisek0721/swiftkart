3050b720a4f8a1ae28c90893d7a38a1e83103836
Abhisekh <abhisekh@WIL-Admins-Mac-mini-3.local>
2024-08-27 19:10:30 +0530

    feat: initial setup

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+export default function About() {
+  return (
+    <div>
+        <Navbar />
+        <div className="mt-20">
+          <h1>About Us</h1>
+        </div>
+    </div>
+  );
+}
