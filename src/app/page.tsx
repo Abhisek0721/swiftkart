049acdcfc7174774c0febf512a4020c1b7b8d71e
Abhisekh Upadhaya <Abhisek0721@gmail.com>
2024-09-20 00:10:13 +0530

    feat: chevron arrow buttons in banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import product from "@/app/data/products.json";
 import categories from "@/app/data/categories.json";
 import banners from "@/app/data/banners.json"
 import Image from "next/image";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi"
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
         {
           categories.map((category, index) => {
             return (
-              <div className="cursor-pointer mx-10 select-none">
+              <div className="cursor-pointer mx-10 select-none" key={index}>
                 <Image className="cursor-pointer mx-auto" src={category.path} alt={category.name} width={78} height={78} />
                 <p className="text-center">{category.name}</p>
               </div>
@@ -23,9 +24,15 @@ export default function Home() {
       </div>
 
       {/* Banner */}
-      <div id="banner" className="w-[80%] h-72 mx-auto rounded-sm mt-5">
+      <div id="banner" className="w-[80%] h-72 mx-auto rounded-sm mt-5 flex items-center light-shadow relative">
+        <div className="absolute left-0 bg-white w-10 h-32 flex items-center justify-center cursor-pointer">
+          <FiChevronLeft className="text-3xl" />
+        </div>
         <div className="cursor-pointer select-none">
           <img className="cursor-pointer mx-auto w-[100%] h-[100%] rounded-sm" src={banners[0].path} alt={banners[0].path} />
+        </div>
+        <div className="absolute right-0 bg-white w-10 h-32 flex items-center justify-center cursor-pointer">
+          <FiChevronRight className="text-3xl" />
         </div>
       </div>
 
